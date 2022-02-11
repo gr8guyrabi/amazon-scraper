445d90016da0cdfd724407afd07672cd3bbb38e9
Rabi Maharjan <gr8guyrabi@gmail.com>
2022-02-11 23:16:40 +0545

    initial setup

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const request = require('request-promise')
+require('dotenv').config()
+
+const app = express()
+
+const PORT = process.env.PORT || 5000
+
+const apiKey = process.env.SCRAPER_API_KEY
+
+app.use(express.json())
+
+app.get('/', (req, res) => {
+    res.send('Welcome to Amazon Scraper API.')
+})
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
